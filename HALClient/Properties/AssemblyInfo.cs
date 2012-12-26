7cf334aa4c7b6a7b1306ec06070d0b94117dbb19
Paul Betts <paul@paulbetts.org>
2012-12-26 00:11:43 -0800

    Change all kinds of stuff
    * Inline the code from Tavis.UriTemplates
    * Switch this to a Portable Library
    * Drop all of the HTTP stuff
    * Switch to NuGet package restore

--- HALClient/Properties/AssemblyInfo.cs
+++ HALClient/Properties/AssemblyInfo.cs
@@ -15,14 +15,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: InternalsVisibleTo("HALClient.Tests")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7f564fe6-f96a-4b88-9a62-567bdd7a5a47")]
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
